acdfdb6c2cf81b660478d731b417ae8341204bb4
SigitHarmadi <55223517+SigitHarmadi@users.noreply.github.com>
2022-12-29 19:00:13 +0700

    first commit

---
+++ engine1/klik-menu-team.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+    $("#tujuan-muncul").click(function(){
+      $(".menu-team").slideToggle();
+    });
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.efek-judul', {
+    duration: 1000,
+    origin:'right',
+    distance:'300px',
+  });
+  sr.reveal('.efek-pendahuluan', {
+    duration: 1000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-box-1', {
+    duration: 1000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-box-2', {
+    duration: 1500,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-box-3', {
+    duration: 2000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-yutub1', {
+    duration: 1000,
+    origin:'left',
+    distance:'300px',
+  });
+  sr.reveal('.efek-yutub2', {
+    duration: 1000,
+    origin:'right',
+    distance:'300px',
+  });
+  sr.reveal('.efek-maps', {
+    duration: 1000,
+    origin:'left',
+    distance:'300px',
+  });
+  sr.reveal('.efek-alamat', {
+    duration: 1000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-manual', {
+    duration: 1000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-matic', {
+    duration: 1000,
+    origin:'top',
+    distance:'300px',
+  });
+  sr.reveal('.efek-kiri1', {
+    duration: 1000,
+    origin:'left',
+    distance:'300px',
+  });
+  sr.reveal('.efek-bawah2', {
+    duration: 1500,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-kanan1', {
+    duration: 1000,
+    origin:'right',
+    distance:'300px',
+  });
+  sr.reveal('.efek-atas2', {
+    duration: 1500,
+    origin:'top',
+    distance:'300px',
+  });
+  sr.reveal('.efek-bawah3', {
+    duration: 2000,
+    origin:'bottom',
+    distance:'300px',
+  });
+  sr.reveal('.efek-tabel1', {
+    duration: 1000,
+    origin:'top',
+    distance:'150px',
+  });
+  sr.reveal('.efek-tabel2', {
+    duration: 1500,
+    origin:'bottom',
+    distance:'100px',
+  });
+  sr.reveal('.efek-tabel3', {
+    duration: 1000,
+    origin:'left',
+    distance:'150px',
+  });
+  sr.reveal('.efek-tabel4', {
+    duration: 1000,
+    origin:'right',
+    distance:'150px',
+  });
